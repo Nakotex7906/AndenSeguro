4348c03263df1d728512c7c25a1ceac93b6433a4
GuillermoSalgado1 <g.salgado04@ufromail.cl>
2026-03-27 15:08:20 -0300

    Inicialización frontend.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to AndenSeguro</h1>
+        <p className="text-lg text-gray-600">Your secure platform for managing your insurance needs.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
